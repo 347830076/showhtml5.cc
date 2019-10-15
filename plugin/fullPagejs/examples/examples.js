333583299ef716ba0dcb3b7901b8a7d7363e64a7
cylyiou <347830076@qq.com>
2019-10-15 14:35:44 +0800

    .

--- plugin/fullPagejs/examples/examples.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function(){
-	$("#demosMenu").change(function(){
-		console.log($(this).find("option:selected").attr("id")+ '.html')
-	  window.location.href = $(this).find("option:selected").attr("id") + '.html';
-	});
-});
